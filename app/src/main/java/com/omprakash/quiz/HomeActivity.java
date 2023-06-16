1b7f1e6438fe2003acec1f242d139148d4deef19
Omprakash <omprakash.mopuri@improve10x.com>
2023-06-16 21:33:41 +0530

    Designed activity_home.xml. Wrote the navigate functionality to QuizActivity.java when click on start btn in HomeActivity.java

--- app/src/main/java/com/omprakash/quiz/HomeActivity.java
+++ app/src/main/java/com/omprakash/quiz/HomeActivity.java
@@ -2,13 +2,27 @@ package com.omprakash.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.omprakash.quiz.databinding.ActivityHomeBinding;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        handleStart();
+    }
+
+    private void handleStart() {
+        binding.startBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(this, QuizActivity.class);
+            startActivity(intent);
+        });
     }
 }
